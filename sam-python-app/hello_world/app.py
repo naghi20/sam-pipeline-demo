e181e955166689c28e47482765533e01f4f11095
MVP Java <mvpjava.email@gmail.com>
2025-06-17 22:04:45 +0000

    kickoff pipline

--- sam-python-app/hello_world/app.py
+++ sam-python-app/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world Version 1.0",
+            "message": "hello world Version 2.0",
             # "location": ip.text.replace("\n", "")
         }),
     }
